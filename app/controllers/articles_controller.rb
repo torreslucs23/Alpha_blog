cfd8b06b0c42208ad4c03923433c876b4a9f5ea7
torreslucs23 <torreslucs23@gmail.com>
2023-08-04 18:00:03 -0300

    added messages validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,23 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
 end
